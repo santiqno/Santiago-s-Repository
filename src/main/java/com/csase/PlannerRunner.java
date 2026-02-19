5ac8664d670035f129c6e364fc291ed76a4e4815
santiqno <26squijano@granvilleschools.org>
2026-02-19 12:57:20 -0500

    Fixed PlannerRunner.java

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -4,10 +4,10 @@ public class PlannerRunner {
     public static void main(String[] args) {
 
         // Create a new student
-        Student java = new Student();
+       Student java = new Student();
 
         // Gets the student's choice until the student exits
-       Student java.getChoice();
+       java.getChoice(); 
 
     }
 }
